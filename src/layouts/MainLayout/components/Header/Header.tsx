1924b79852e516079ebb6e80d6b36742e05dd9a1
Sudip Das <das366966@gmail.com>
2024-03-10 19:23:43 +0530

    Auto emailer client ui

--- src/layouts/MainLayout/components/Header/Header.tsx
+++ src/layouts/MainLayout/components/Header/Header.tsx
@@ -1,5 +1,4 @@
-import { Layout } from 'antd'
-import React from 'react'
+import { Avatar, Layout } from 'antd'
 import { useSelector } from 'react-redux'
 
 export default function Header() {
@@ -8,7 +7,17 @@ export default function Header() {
     <Layout.Header
           className='w-[calc(100%-var(--sidebar-width))] fixed z-50 top-0 right-0 bg-white h-[var(--header-height)] border border-border-on-light transition-all duration-300 ease-in-out flex items-center justify-between px-4 shadow-sm' 
     >
-      {headerDetails.name}
+      <div className="flex justify-between w-full">
+        <h2>{headerDetails.name}</h2>
+        <div className='flex gap-2 items-center'>
+          <Avatar size={40} src="https://api.dicebear.com/7.x/lorelei/svg" />
+          <div className='flex flex-col' style={{lineHeight:1.5}}>
+            <h3>John Doe</h3>
+            <p className="text-gray-500">Admin</p>
+          </div>
+        </div>
+      </div>
+      
     </Layout.Header>
   )
 }
